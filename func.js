1c912ac6633d2add7ce14bc2fdbddef978b40f12
muhammad owais shah <syedowaisshah0101@gmail.com>
2024-05-15 16:31:02 +0500

    inital

---
+++ func.js
@@ -0,0 +1,36 @@
+"use strict";
+// function is block of code that use to spacific task.
+// tow types of functiions.
+//static function
+//dynamic function
+//static function
+function func_static() {
+    console.log("my name is muhammad owias shah");
+}
+func_static();
+//this is a static function
+//dynamic function 
+// which function is take parameter and arguments is called dynamic function
+function func_dynamic(fname, lName) {
+    console.log(` How are you ${fname}${lName} `);
+}
+func_dynamic("muhammad", "owais shah");
+//there are few types of functions
+// ananymous function 
+// anonymouis function is also called without name function
+let fun_anonmyous = function () {
+    console.log("how are you");
+};
+fun_anonmyous();
+// 2nd one is naming function
+// naming function is that contain name is called naming function
+function fun_naming() {
+    console.log("whats is your name");
+}
+fun_naming();
+//arrow function
+// arrow function is also called lamda or anonmyous function
+let fun_arrow = (name, age) => {
+    console.log(`myname is ${name} and i am ${age} old`);
+};
+fun_arrow("muhammad owais shah", 18);
